9590c88a462a910ec15b7949c04d8c5543c54204
Yossi Shwartz <yossi.shwartz@dynamicyield.com>
2023-09-18 18:04:25 +0300

    adding tailwind, ts and build scripts

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from 'react';
+import Link from 'next/link';
+import './globals.css';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about" prefetch={false}>About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          Game data and images courtesy
+          of <a href="https://rawg.io/" target="_blank">RAWG</a>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    console.log('[HomePage] rendering');
+    return (
+      <>
+        <h1>Indie Gamer</h1>
+        <p>
+          Only the best indie games, reviewed for you.
+        </p>
+      </>
+    );
+  }
